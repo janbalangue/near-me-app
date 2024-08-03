1f242a39e78793f274ec229114afcbbcab24dbc5
Jan Balangue <jan.a.balangue@gmail.com>
2024-08-03 16:34:35 -0700

    Add uuid

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,13 +2,14 @@ import { Container } from "reactstrap";
 import { response } from "../data/response";
 import SearchFilterForm from "../features/search/SearchFilterForm";
 import SearchRow from "../features/search/SearchRow";
+import { v4 as uuid } from "uuid";
 
 const SearchPage = () => {
   return (
     <Container fluid>
       <SearchFilterForm />
-      {response.places.map((site, index) => {
-        return <SearchRow key={index} site={site} />;
+      {response.places.map((site) => {
+        return <SearchRow key={uuid()} site={site} />;
       })}
     </Container>
   );

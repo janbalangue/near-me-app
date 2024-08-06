54a4a3fd97d125f0d942a933ff3c422b73c11732
Jan Balangue <jan.a.balangue@gmail.com>
2024-08-05 19:50:34 -0700

    Implement search filter

--- src/features/search/SearchRow.js
+++ src/features/search/SearchRow.js
@@ -1,22 +1,22 @@
 import { Card, CardTitle, CardBody, CardText, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const SearchRow = ({ site, index }) => {
+const SearchRow = ({ place, index }) => {
   return (
     <Row key={index} style={{ margin: "3rem" }}>
-      <Card className="site-card">
+      <Card className="place-card">
         <CardTitle className="m-3">
-          <h4>{site.displayName.text}</h4>
+          <h4>{place.displayName.text}</h4>
         </CardTitle>
         <CardBody>
-          <CardText>{site.formattedAddress}</CardText>
+          <CardText>{place.formattedAddress}</CardText>
           <CardText>
-            {site.priceLevel
-              ? site.priceLevel.split("_").join(" ").toLowerCase()
+            {place.priceLevel
+              ? place.priceLevel.split("_").join(" ").toLowerCase()
               : "price level unavailable"}
             <br />
             <br />
-            <Link to={site.websiteUri}>
+            <Link to={place.websiteUri}>
               <i className="fa fa-globe" aria-hidden="true"></i>
               Go to website
             </Link>
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,23 +1,53 @@
-import { Container } from "reactstrap";
-import SearchFilterForm from "../features/search/SearchFilterForm";
+import { Container, Input } from "reactstrap";
 import SearchRow from "../features/search/SearchRow";
 import { v4 as uuid } from "uuid";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { response } from "../data/response";
 
 const SearchPage = () => {
+  const [filterText, setFilterText] = useState("");
+  const [searchRows, setSearchRows] = useState(response.places);
   useEffect(() => {
     document.title = "Near Me App | Search";
   }, []);
+  useEffect(() => {
+    setSearchRows(
+      response.places.filter((place) => {
+        const [name, address, priceLevel] = [
+          place.displayName.text.toLowerCase(),
+          place.formattedAddress.toLowerCase(),
+          place.priceLevel
+            ? place.priceLevel.split("_").join(" ").toLowerCase()
+            : "price level unavailable",
+        ];
+        return (
+          name.includes(filterText.toLowerCase()) ||
+          address.includes(filterText.toLowerCase()) ||
+          priceLevel.includes(filterText.toLowerCase())
+        );
+      })
+    );
+  }, [filterText]);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFilterText(e.target.value);
+  };
   return (
     <Container fluid>
-      <SearchFilterForm />
-      {response.places.map((site) => {
-        return <SearchRow key={uuid()} site={site} />;
+      <Input
+        id="filter"
+        name="filter"
+        placeholder="Type here to filter the places below"
+        type="search"
+        className="border"
+        onChange={handleChange}
+      />
+      {searchRows.map((place) => {
+        return <SearchRow key={uuid()} place={place} />;
       })}
+      <h3 className="mt-5 text-center">No results found</h3>
     </Container>
   );
-
 };
 
 export default SearchPage;

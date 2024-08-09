c14632f7e72ce6a5dc51ed3a2cd85755ec977da2
Jan Balangue <jan.a.balangue@gmail.com>
2024-08-09 09:09:11 -0700

    Add formik, yup, error messages, button css

--- src/features/home/HomeSearchForm.js
+++ src/features/home/HomeSearchForm.js
@@ -1,40 +1,65 @@
-import { Container, Col, FormGroup, Button, Label, Input } from "reactstrap";
+import { Container, Col, FormGroup, Label } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const SearchSchema = Yup.object().shape({
+  search: Yup.string()
+    .min(2, "Please enter at least 2 characters")
+    .required("Required"),
+});
 
 const HomeSearchForm = () => {
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate(`/search/${e.target.search.value}`);
+  const handleSubmit = (values) => {
+    navigate(`/search/${values.search}`);
   };
   return (
     <Container id="searchContainer" fluid>
-      <form onSubmit={handleSubmit}>
-        <FormGroup>
-          <Col>
-            <Label htmlFor="search">Search</Label>
-            <Input
-              className="form-control"
-              id="search"
-              name="search"
-              placeholder="Where to?"
-              type="search"
-              autoFocus
-            />
-          </Col>
-        </FormGroup>
-        <div className="searchBtn">
-          {" "}
-          <Button
-            type="submit"
-            name="submit"
-            className="searchBtn px-5"
-            color="primary"
-          >
-            Submit
-          </Button>
-        </div>
-      </form>
+      <Formik
+        initialValues={{ search: "" }}
+        validationSchema={SearchSchema}
+        onSubmit={( values, {setSubmitting}) => {
+          console.log("values", values);
+          handleSubmit(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <FormGroup>
+              <Col>
+                <Label htmlFor="search">Search</Label>
+                <Field
+                  className="form-control"
+                  id="search"
+                  name="search"
+                  placeholder="Where to?"
+                  type="search"
+                  autoFocus
+                />
+                <ErrorMessage
+                  name="search"
+                  component="div"
+                  className="error-message"
+                />
+              </Col>
+            </FormGroup>
+            <Container className="searchBtnContainer">
+              {" "}
+              <button
+                type="submit"
+                name="submit"
+                className="searchBtn px-5"
+                color="primary"
+                disabled={isSubmitting}
+              >
+                Submit
+              </button>
+            </Container>
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 };

9f2665bbd2d803bacf7d6e1478528b3e25025bfe
Jan Balangue <jan.a.balangue@gmail.com>
2024-08-08 17:27:31 -0700

    change AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,8 +10,8 @@ const AboutPage = () => {
       <h1 className="mt-5 text-center">About This App</h1>
       <div className="mt-5">
         <p className="mx-5 text-center">
-        This is a React portfolio project created at NuCamp. It provides points of interest
-         from a Spring Boot REST API that calls the Google Maps Places API.
+        This is a React portfolio project created for NuCamp. It provides points of interest
+         from a Java/Spring Boot REST API that calls the Google Maps Places API.
         </p>
         <p className="mx-5 text-center">
         Under the hood, the Places API uses geolocation.

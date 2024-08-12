72db858679a06a1e36384fd5881ad3afb5a7fffe
Jan Balangue <jan.a.balangue@gmail.com>
2024-08-12 09:14:45 -0700

    remove comments

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,7 +3,6 @@ import SearchRow from "../features/search/SearchRow";
 import { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import { useParams } from "react-router-dom";
-// import { callPlacesBackend } from "../data/callPlacesBackend";
 import NoResults from "../features/search/NoResults";
 import { useSpring, animated } from "react-spring";
 import { response } from "../data/response";
@@ -17,26 +16,12 @@ const SearchPage = () => {
     config: { duration: 500 },
   });
   const [places, setPlaces] = useState([]);
-  // const storedPlacesRef = useRef(null);
   const [filterText, setFilterText] = useState("");
   useEffect(() => {
     document.title = `Near Me App | Search | ${query}`;
     setPlaces(response.places);
     setToggle(true);
   }, [query]);
-  // useEffect(() => {
-  //   const fetchPlaces = async () => {
-  //     try {
-  //       const response = await callPlacesBackend(query);
-  //       setPlaces(response);
-  //       storedPlacesRef.current = response;
-  //       setToggle(true);
-  //     } catch (error) {
-  //       console.error(error.message);
-  //     }
-  //   };
-  //   fetchPlaces();
-  // }, [query]);
   const handleChange = (e) => {
     e.preventDefault();
     setFilterText(e.target.value);

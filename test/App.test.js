818b6a7e98a994517a06e86b0c6490ed9d39b377
Jan Balangue <jan.a.balangue@gmail.com>
2025-09-08 13:15:03 -0700

    add unit test

--- test/App.test.js
+++ test/App.test.js
@@ -1,1 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../src/App';
 
+test('renders Near Me App link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Near Me App/i);
+  expect(linkElement).toBeInTheDocument();
+});

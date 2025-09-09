1f3d7ac8fb9e0133f4c493bcba1e5b1c6a3f88f6
Jan Balangue <jan.a.balangue@gmail.com>
2025-09-09 14:24:02 -0700

    Add .prettierrc and reformat first-test.cy.js

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -1,16 +1,16 @@
-describe("home page tests", () => {
-  it("passes", () => {
-    cy.visit("https://janbalangue.github.io/near-me-app/");
-    cy.get(".App").should("be.visible");
-    cy.get(".appTitle").contains("Near Me App");
-    cy.get(".App").contains("Find your next favorite place.");
-    cy.get(".App").contains("Search");
-    cy.get(".navbar").should("be.visible");
-    cy.get(".navbar").contains("Home");
-    cy.get(".navbar").contains("About");
-    cy.get(".site-footer").should("be.visible");
-    cy.get(".site-footer").contains("Links");
-    cy.get(".site-footer").contains("Home");
-    cy.get(".site-footer").contains("About");
+describe('home page', () => {
+  it('passes', () => {
+    cy.visit('https://janbalangue.github.io/near-me-app/');
+    cy.get('.App').should('be.visible');
+    cy.get('.appTitle').contains('Near Me App');
+    cy.get('.App').contains('Find your next favorite place.');
+    cy.get('.App').contains('Search');
+    cy.get('.navbar').should('be.visible');
+    cy.get('.navbar').contains('Home');
+    cy.get('.navbar').contains('About');
+    cy.get('.site-footer').should('be.visible');
+    cy.get('.site-footer').contains('Links');
+    cy.get('.site-footer').contains('Home');
+    cy.get('.site-footer').contains('About');
   });
 });

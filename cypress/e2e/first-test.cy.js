e05cc37694c9d95079d25dd55744105f0e9fadaf
Jan Balangue <jan.a.balangue@gmail.com>
2025-09-09 13:49:52 -0700

    Add more home page tests

--- cypress/e2e/first-test.cy.js
+++ cypress/e2e/first-test.cy.js
@@ -1,6 +1,16 @@
-describe('template spec', () => {
+describe('home page tests', () => {
   it('passes', () => {
     cy.visit('https://janbalangue.github.io/near-me-app/')
+    cy.get('.App').should('be.visible')
+    cy.get('.App').contains('Find your next favorite place.')
+    cy.get('.App').contains('Search')
+    cy.get('.navbar').should('be.visible')
+    cy.get('.navbar').contains('Home')
+    cy.get('.navbar').contains('About')
     cy.get('.appTitle').contains('Near Me App')
+    cy.get('.site-footer').should('be.visible')
+    cy.get('.site-footer').contains('Links')
+    cy.get('.site-footer').contains('Home')
+    cy.get('.site-footer').contains('About')
   })
 })
